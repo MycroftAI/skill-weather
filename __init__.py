c7f05b3dcbdadc3c781c1142688320324037782c
Tomasz Rozynek <tomasz.rozynek@dir.pl>
2020-09-21 23:25:51 +0200

    Added support for Polish language. Still in progress. Fixed parsing "today" relative day as local language expression, instead of forced English.

--- __init__.py
+++ __init__.py
@@ -1262,7 +1262,7 @@ class WeatherSkill(MycroftSkill):
     def __populate_report(self, message):
         unit = self.__get_requested_unit(message)
         # Get a date from requests like "weather for next Tuesday"
-        today, _ = self.__extract_datetime("today")
+        today, _ = self.__extract_datetime("today", lang='en-us')
         when, _ = self.__extract_datetime(
                     message.data.get('utterance'), lang=self.lang)
         when = when or today # Get todays date if None was found
