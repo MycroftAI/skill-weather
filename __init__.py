b80e5adc93bdd87a4f5f8a3bb23cabc50509967f
Åke Forslund <ake.forslund@gmail.com>
2021-12-12 10:16:00 +0100

    Fix "when will it snow" when not forcasted
    This will not perform translation of percentage when the result doesn't
    contain percentage to translate.

--- __init__.py
+++ __init__.py
@@ -519,10 +519,15 @@ class WeatherSkill(MycroftSkill):
             dialog_args = intent_data, self.weather_config, forecast
             dialog = get_dialog_for_timeframe(intent_data.timeframe, dialog_args)
             dialog.build_next_precipitation_dialog()
-            spoken_percentage = self.translate(
-                "percentage-number", data=dict(number=dialog.data["percent"])
-            )
-            dialog.data.update(percent=spoken_percentage)
+
+            # Percent is not available if, for example, there is no
+            # precipitation in the the next 7 days.
+            if "percent" in dialog.data:
+                spoken_percentage = self.translate(
+                    "percentage-number",
+                    data=dict(number=dialog.data["percent"])
+                )
+                dialog.data.update(percent=spoken_percentage)
             self._speak_weather(dialog)
 
     @intent_handler(
