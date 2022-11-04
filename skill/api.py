b527751329830f3ec40b21e4381b3bc0bcf5e482
Aduen Darriba Frederiks <aduen.darribafrederiks@ugent.be>
2022-11-04 16:18:57 +0100

    Fixed broken dialog and added additional dialog and vocabulary

--- skill/api.py
+++ skill/api.py
@@ -22,8 +22,6 @@ It also supports returning values in the measurement system (Metric/Imperial)
 provided, precluding us from having to do the conversions.
 
 """
-import sys
-
 from mycroft.api import Api
 from .weather import WeatherReport
 
@@ -128,6 +126,5 @@ class OpenWeatherMapApi(Api):
         api_request = dict(path="/onecall", query=query_parameters)
         response = self.request(api_request)
         local_weather = WeatherReport(response)
-        for line in response:
-            sys.stdout.write(str(line))
+
         return local_weather
